4111a78ac54b59c6242eb71f6168f015e5cd9ad3
Gil Matzafi <gillm9136@gmail.com>
2025-04-17 16:14:40 +0300

    integrate bsign up between frontend and backend

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -3,12 +3,14 @@ from sqlalchemy.orm import Session
 from app.controllers.auth_controller import register_user, login_user, refresh_access_token
 from database.database import get_db
 from fastapi.security import OAuth2PasswordRequestForm
+from app.schemas.auth_schemas import RegisterRequest
+
 
 router = APIRouter()
 
-@router.post("/register")
-def register(username: str, password: str, email: str, db: Session = Depends(get_db)):
-    return register_user(db, username, password, email)
+@router.post("/register", status_code=201)
+def register(register_data: RegisterRequest, db: Session = Depends(get_db)):
+    return register_user(db, register_data.username, register_data.password, register_data.email)
 
 @router.post("/login")
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
