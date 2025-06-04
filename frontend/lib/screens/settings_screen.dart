adedf4f88baf34069698a0e3ad08913691247ba2
Gil Matzafi <gillm9136@gmail.com>
2025-06-04 13:22:02 +0300

    delete unnecceary things in setting screen

--- frontend/lib/screens/settings_screen.dart
+++ frontend/lib/screens/settings_screen.dart
@@ -29,10 +29,6 @@ class _SettingsScreenState extends State<SettingsScreen> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.home),
-          onPressed: () => Navigator.pop(context),
-        ),
         title: const SettingsAppBarTitle(),
         centerTitle: true,
         toolbarHeight: 90,
@@ -49,39 +45,6 @@ class _SettingsScreenState extends State<SettingsScreen> {
             ],
           ),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 3,
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.white,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.camera_alt), label: 'Camera'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.notifications), label: 'Alerts'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings), label: 'Settings'),
-        ],
-        onTap: (index) {
-          if (index == 0) {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => HomeScreen(username: babyName)),
-            );
-          } else if (index == 2) {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => AlertsScreen(notifications: [])),
-            );
-          } else if (index == 3) {
-            // Already on settings, do nothing
-          }
-        },
       ),
     );
   }
